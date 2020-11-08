b012ee5c4f8a8bba3fcfd32f7fd3198ebd41b56e
Mykola Zahanych <kolyazaganych98@gmail.com>
2020-11-08 19:06:45 +0100

    get user profile && update profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { authUser, registerUser, getUserProfie } from '../controllers/userController.js';
+import { authUser, registerUser, getUserProfie, updateUserProfie } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', authUser);
 router.route('/').post(registerUser);
-router.route('/profile').get(protect, getUserProfie);
+router.route('/profile').get(protect, getUserProfie).put(protect, updateUserProfie);
 
 export default router;
