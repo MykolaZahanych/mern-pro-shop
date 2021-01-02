846fdf535258b4dd4fcd89f15330e7e478553e6c
Mykola Zahanych <kolyazaganych98@gmail.com>
2021-01-02 20:12:12 +0100

    admin order

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -18,7 +18,14 @@ import {
   userDeleteReducer,
   userUpdateReducer
 } from './reducers/userReducers';
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer } from './reducers/orderReducers';
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderListReducer,
+  orderPayReducer,
+  orderDeliverReducer,
+  orderListMyReducer
+} from './reducers/orderReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -37,7 +44,9 @@ const reducer = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
-  orderListMy: orderListMyReducer
+  orderDeliver: orderDeliverReducer,
+  orderListMy: orderListMyReducer,
+  orderList: orderListReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
